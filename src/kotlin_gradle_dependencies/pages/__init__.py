856e999101f389ae05a31bfc24ed3795e6b894bd
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-09 16:20:15 +0300

    Small fix

--- src/kotlin_gradle_dependencies/pages/__init__.py
+++ src/kotlin_gradle_dependencies/pages/__init__.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Type
 
 from src.common.model import Page
 from src.kotlin_gradle_dependencies.pages.per_module_stats_for_all_projects import PerModuleStatsForAllProject
@@ -7,8 +7,8 @@ from src.kotlin_gradle_dependencies.pages.per_module_stats_without_android_proje
 )
 from src.kotlin_gradle_dependencies.pages.per_project_stats import PerProjectStats
 
-PAGE_MAP: Dict[str, Page] = {
-    'Per module statistics for all projects': PerModuleStatsForAllProject(),
-    'Per module statistics without Android projects': PerModuleStatsWithoutAndroidProjects(),
-    'Per project statistics': PerProjectStats(),
+PAGE_MAP: Dict[str, Type[Page]] = {
+    'Per module statistics for all projects': PerModuleStatsForAllProject,
+    'Per module statistics without Android projects': PerModuleStatsWithoutAndroidProjects,
+    'Per project statistics': PerProjectStats,
 }
