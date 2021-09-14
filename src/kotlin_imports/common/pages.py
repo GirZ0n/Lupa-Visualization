5e01c2d8f0890ba432ebf616920be1d441458b59
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-09-14 18:07:56 +0300

    Add ColumnName

--- src/kotlin_imports/common/pages.py
+++ src/kotlin_imports/common/pages.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 
 from src.common.fragments import show_bar_plot_with_config
+from src.kotlin_imports.common.column_name import ColumnName
 
 
 def show_page(
@@ -20,9 +21,9 @@ def show_page(
         header='The occurrence of imports FQ names',
         description='The occurrence of the mined import directives / fully qualified names from projects.',
         df=import_stats,
-        x_axis='fq_name',
+        x_axis=ColumnName.FQ_NAME.value,
         x_title='Import name',
-        y_axis='count',
+        y_axis=ColumnName.COUNT.value,
         y_title='Count',
         key=f'{key}_import_stats',
     )
@@ -34,9 +35,9 @@ def show_page(
             'heuristics (number of child nodes, number of leaves, occurrence, depth, size of the subtree).'
         ),
         df=import_stats_by_package,
-        x_axis='fq_name',
+        x_axis=ColumnName.FQ_NAME.value,
         x_title='Package name',
-        y_axis='count',
+        y_axis=ColumnName.COUNT.value,
         y_title='Count',
         key=f'{key}_import_stats_by_package',
     )
