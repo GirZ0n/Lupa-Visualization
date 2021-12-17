deedfa02f779a2b8edb4a40101b30a93eb53100e
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-12-17 14:13:44 +0300

    Add StatsWithoutStdlibAndBuiltins

--- src/python_call_expressions/pages/__init__.py
+++ src/python_call_expressions/pages/__init__.py
@@ -2,8 +2,9 @@ from typing import Dict, Type
 
 from src.common.model import Page
 from src.python_call_expressions.pages.stats_for_all_call_expressions import StatsForAllCallExpressions
+from src.python_call_expressions.pages.stats_without_stdlib_and_builtins import StatsWithoutStdlibAndBuiltins
 
 PAGE_MAP: Dict[str, Type[Page]] = {
     'Statistics for all imports': StatsForAllCallExpressions,
-    'Statistics without stdlib': StatsForAllCallExpressions,  # TODO
+    'Statistics without stdlib': StatsWithoutStdlibAndBuiltins,
 }
