b83d416ee26e262fad286d28c5cda08b558325a3
GirZ0n <ilyavlasov2011@gmail.com>
2022-06-30 14:06:49 +0300

    Architectural changes: Streamlit now creates multipage applications

---
+++ src/python_imports/pages/1_Statistics_for_all_imports.py
@@ -0,0 +1,19 @@
+from src.common.utils import get_stats_by_name
+from src.python_imports import DATA_FOLDER
+from src.python_imports.common.pages import show_page
+
+import streamlit as st
+
+if __name__ == '__main__':
+    st.set_page_config(page_title='Python Imports', layout='wide')
+
+    show_page(
+        title='Statistics for all imports',
+        description=(
+            "We mined all *imports' fully qualified names* from each Python file "
+            'in the dataset using PSI (see `/resources/python_imports/data/import_data.csv`).'
+        ),
+        import_stats_by_version=get_stats_by_name(DATA_FOLDER / 'all' / 'import_stats'),
+        package_stats_by_version=get_stats_by_name(DATA_FOLDER / 'all' / 'import_stats_by_package'),
+        key='all',
+    )
